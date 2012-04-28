dc52cd68239549a12c02f9bad216cf274b702505
modocache <modocache@gmail.com>
2012-04-29 04:52:49 +0900

    Update project for iOS SDK 5.

--- Classes/GameViewController.m
+++ Classes/GameViewController.m
@@ -1,10 +1,26 @@
 //
-//  GameViewController.m
-//  TapTap
+//  Copyright (c) 2012 modocache
 //
-//  Created by Brian Ivan Gesiak on 5/5/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
 //
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 
 #import "GameViewController.h"
 #import "ResultViewController.h"
@@ -270,12 +286,14 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.counterLabel setFont:[UIFont fontWithName:@"Silom" size:112.0]];
 	
 	for (UIView *v in self.view.subviews) {
 		if ([v isKindOfClass:[UILabel class]]) {
-		  UILabel *label = (UILabel*)v;
-
-		  [label setFont:[UIFont fontWithName:@"Silom" size:label.font.pointSize]];
+            UILabel *label = (UILabel*)v;
+            NSLog(@"%@:%@ -- label.font.pointSize[%f]", [self class], NSStringFromSelector(_cmd), label.font.pointSize);
+            [label setFont:[UIFont fontWithName:@"Silom" size:label.font.pointSize]];
 		}
 	}
 	
--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -1,10 +1,26 @@
 //
-//  RootViewController.m
-//  TapTap
+//  Copyright (c) 2012 modocache
 //
-//  Created by Brian Ivan Gesiak on 5/5/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
 //
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 
 #import "RootViewController.h"
 #import "MainMenuViewController.h"
@@ -15,20 +31,6 @@
 
 @synthesize mainMenuViewController;
 
-
-// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-/*
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization.
-    }
-    return self;
-}
-*/
-
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
 	MainMenuViewController *mMViewController = [[MainMenuViewController alloc] initWithNibName: @"MainMenuView" 
 																							  bundle: nil];
@@ -38,15 +40,6 @@
 	[mMViewController release];
     [super viewDidLoad];
 }
-
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
 
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
